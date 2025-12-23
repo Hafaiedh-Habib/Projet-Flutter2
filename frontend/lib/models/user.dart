fa0081ec392e3538290dfb8c3f6b5988d1be33e9
Hafaiedh-Habib <hafaiedh.habib11@gmail.com>
2025-12-23 13:16:03 +0100

    create models
    préparer les deux modeles ( user - person)

---
+++ frontend/lib/models/user.dart
@@ -0,0 +1,35 @@
+class User {
+  final int? id;
+  final String nom;
+  final String prenom;
+  final String numero;
+  final String motDePasse;
+
+  User({
+    this.id,
+    required this.nom,
+    required this.prenom,
+    required this.numero,
+    required this.motDePasse,
+  });
+
+  factory User.fromJson(Map<String, dynamic> map) {
+    return User(
+      id: map['id'],
+      nom: map['nom'],
+      prenom: map['prenom'],
+      numero: map['numero'],
+      motDePasse: map['mot_de_passe'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'nom': nom,
+      'prenom': prenom,
+      'numero': numero,
+      'mot_de_passe': motDePasse,
+    };
+  }
+}
